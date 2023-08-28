e9510d805e57b4fed305156f3ba91fa732ff4b73
jungbin97 <ljb5054@gmail.com>
2023-08-28 12:02:39 +0900

    top-down고려

--- Week3/Solution.py
+++ Week3/Solution.py
@@ -23,3 +23,31 @@ if dp[N] == float('inf'):
     print(-1)
 else:
     print(dp[N])
+    
+# -------------------------------------------------------------------------
+# top-down방식 시간 초과(test Case 5~24번)
+# 재귀 + Dp 방식으로 풀어보려고 했으나 시간 초과로 Fail
+# 식을 입력하면 계산에 대한 내용은 컴퓨터에게 맡기니 코드는 보기 좋으나 stack overflow 때문에
+# 코딩 테스트에서는 최대한 지양해야겠다...
+# -------------------------------------------------------------------------
+import math
+import sys
+# 입력 받기
+N = int(input())
+Ap, Bp = map(int, input().split())
+dp = [float('inf')] * (10**6+1)
+
+def solve(pain):
+    if pain == 0:
+        return 0
+    if pain < 0:
+        return float('inf')
+    
+    dp[pain] = min(solve(pain - Ap), solve(pain - Bp)) + 1
+    return dp[pain]
+
+result = solve(N)
+if result == float('inf'):
+    print(-1)
+else:
+    print(result)
